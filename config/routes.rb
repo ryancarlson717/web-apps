aee4626cab166c81d55d831274dcddf5b43789d6
Ryan Carlson <ryan.carlson1@kellogg.northwestern.edu>
2024-05-03 16:32:52 +0000

    Friday practice

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # get("/things", :controller => "things", :action => "index")
   get("/tacos", :controller => "tacos", :action => "index")
+  get("/dice", :controller => "dice", :action => "index")
+  #shorthand is rseources "companies"
+  get("/companies", :controller => "companies", :action => "index")
+  get("/contacts", :controller => "contacts", :action => "index")
 end
